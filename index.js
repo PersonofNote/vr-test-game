8afca3e94232154b0549f48a8fb429aa48f9ce19
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-06-16 15:20:36 -0700

    Implemented some simple on-click animations

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/*
--Create list of available spaces. 
--Populate list with objects. 
--Assign one the attribute of Mimic. 
--Implement controls such the the player may interact with an object
-	-Pointing controller at it should highlight the object
-	-Trigger or button interaction should either make a guess, or ask if you want to make a guess
--Probably not picking up or anything, just examining.
--Implement dialogue box with choice of 'accuse' or 'close' on interact. 
--Implement descriptions with variables. 
--Implement AI for the mimic object
-	-When the camera is not pointed at the mimic, occasionally it should move a little or change something about itself.
--Consider having the lighting come from the player's off hand, in the form of a lantern they can direct.
--Implement logic for guessing
-	-If the selected object is a mimic, player wins.
-	     -Win state could be forcing the mimic into its natural state and capturing it in a jar. Cute.
-	-else if remaining guesses > 0 && time left > 0, guesses -- and close dialog box.
-		    -Consider marking the object so that they player does not waste guesses on the same object by accident
-	-else if no guesses || no time, lose. Mimic eats or otherwise wins against player.
-*/
 var sceneEl = document.querySelector('a-scene');
 //Eventually this in theory will be replaced by some kind of materials colorset.
 var colors = ["green", "yellow", "orange", "red", "blue", "purple"];
@@ -35,8 +15,18 @@ function createEntities() {
   		radius: 0.1
 		});
 	mimicEl.setAttribute('is-mimic', true);
-	mimicEl.setAttribute('material', 'color', 'purple');
-	mimicEl.setAttribute('position', {x: -2, y: 1, z: -4});
+	mimicEl.setAttribute('material', 'color', 'orange');
+	mimicEl.setAttribute('position', {x: -1, y: 1, z: -1});
+	 mimicEl.addEventListener('click', function (evt) {
+  		  if (mimicEl.getAttribute('is-mimic') == true) {
+			    alert("YOU FOUND IT");
+			  }
+			  else {
+			    guesses -= 1;
+			    alert("Nope, try again");
+			    console.log("You have " + guesses);
+			  }
+  			});
 	//Add other entities
 	for (var i = 0; i < 12; i++) {
 	var el = document.createElement('a-entity');
@@ -55,10 +45,9 @@ function createEntities() {
   });
   el.setAttribute('material', 'color', shade);
   el.setAttribute('position', {x: xpos, y:ypos, z: -3});
-  //As per the docs, click doesn't work this way for 3d elements.
   el.addEventListener('click', function (evt) {
-  		  if ("is-mimic" == true) {
-			    alert("RAR YOU FOUND IT");
+  		  if (el.getAttribute('is-mimic') == true) {
+			    alert("YOU FOUND IT");
 			  }
 			  else {
 			    guesses -= 1;
@@ -66,7 +55,6 @@ function createEntities() {
 			    console.log("You have " + guesses);
 			  }
   			});
-	console.log('Blank entity ' + i + ' added.');
 	console.log(el.click);
 }
 }
@@ -100,13 +88,14 @@ AFRAME.registerComponent('is-mimic', {
   schema: {type: 'string'},
   init: function () {
     var stringToLog = this.data;
-    console.log("Hello, my mimic status is " + stringToLog);
+    console.log("Hello, I am " + this + " and my mimic status is " + stringToLog);
   }
 });
 
 AFRAME.registerComponent('on-load', {
   init: function () {
     // This will be called after the entity has properly attached and loaded.
+    //For some reason setting the attributes isn't working from this function. Explore more.
       /*
       el.setAttribute('geometry', {
   		primitive: 'cylinder',
@@ -118,13 +107,13 @@ AFRAME.registerComponent('on-load', {
 	//el.object3D.position.set(1, 2, 3);
   */
 	
-    console.log('I am ready!');
   }
 });
 
+/*
 function checkIfmimic() {
   if ("is-mimic" == true) {
-    alert("RAR YOU FOUND IT");
+    alert("YOU FOUND IT");
   }
   else {
     guesses -= 1;
@@ -132,6 +121,7 @@ function checkIfmimic() {
     console.log("You have " + guesses);
   }
 }
+*/
 
 
 /*
@@ -150,4 +140,3 @@ function appendObject(id, file, scale, position, rotation, scale) {
        document.getElementById(id).setAttribute("position", position); // this does set position as a workaround
       }
       */
-
